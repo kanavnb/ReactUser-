c788e00c47e1ed4f8b13c4264ee8ec3cc6188b6a
kanavnb <kanatbek.kamilov@alatoo.edu.kg>
2024-11-18 16:33:21 +0600

    Show message when button is clicked

--- my-react-project/src/App.jsx
+++ my-react-project/src/App.jsx
@@ -1,12 +1,13 @@
-import './App.css';
-import PersonInfo from './PersonInfo';
-
-function App() {
+const PersonInfo = () => {
   return (
-    <div className="App">
-      <PersonInfo />
+    <div id="person-info" className="person-info">
+      <h1>Name: Ivan Ivanov</h1>
+      <p>Position: Developer</p>
+      <p>Company: TechCorp</p>
+      <p>City: Bishkek</p>
+      <p>Age: 25</p>
+      <p>Hobbies: Programming</p>
+      <button id="change-position-button">Click Me</button>
     </div>
   );
-}
-
-export default App;
+};
--- my-react-project/src/PersonInfo.jsx
+++ my-react-project/src/PersonInfo.jsx
@@ -1,14 +1,22 @@
+import { useState } from 'react';
+
 const PersonInfo = () => {
+    const [message, setMessage] = useState('');
+
+    const handleClick = () => {
+        setMessage('Message: Hello!');
+    };
+
     return (
-      <div id="person-info" className="person-info">
-        <h1>Name: Ivan Ivanov</h1>
-        <p>Position: Developer</p>
-        <p>Company: TechCorp</p>
-        <p>City: Bishkek</p>
-        <p>Age: 25</p>
-        <p>Hobbies: Programming</p>
-        <button id="change-position-button">Click Me</button>
-      </div>
+        <div id="person-info" className="person-info">
+            <h1>Name: Ivan Ivanov</h1>
+            <p>Position: Developer</p>
+            <p>Company: TechCorp</p>
+            <p>City: Bishkek</p>
+            <p>Age: 25</p>
+            <p>Hobbies: Programming</p>
+            <button id="change-position-button" onClick={handleClick}>Click me</button>
+            {message && <p>{message}</p>}
+        </div>
     );
-  };
-  
+};
