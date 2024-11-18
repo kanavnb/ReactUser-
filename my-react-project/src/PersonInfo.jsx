0eeb94c87343a8eb6e605a12983a3b26cfc5412d
kanavnb <kanatbek.kamilov@alatoo.edu.kg>
2024-11-18 16:24:48 +0600

    Add button to PersonInfo component

---
+++ my-react-project/src/PersonInfo.jsx
@@ -0,0 +1,14 @@
+const PersonInfo = () => {
+    return (
+      <div id="person-info" className="person-info">
+        <h1>Name: Ivan Ivanov</h1>
+        <p>Position: Developer</p>
+        <p>Company: TechCorp</p>
+        <p>City: Bishkek</p>
+        <p>Age: 25</p>
+        <p>Hobbies: Programming</p>
+        <button id="change-position-button">Click Me</button>
+      </div>
+    );
+  };
+  
